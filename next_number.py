b62fff61e450c63ee66df7f92078b6d3d577742b
Annie Pulzone <ajpulzone@gmail.com>
2024-01-06 08:42:31 -0800

    Created a function that adds 1 to the number argument taken and returns that number

---
+++ next_number.py
@@ -0,0 +1,15 @@
+#Return the next number from the integer passed
+#Create a function that takes a number as an argument, increments the unmber by +1 and returns the result
+
+# Examples:
+# addition(0) -> 1
+# addition(9) -> 10
+# addition(-3) -> -2
+
+def add_one(num1):
+  return num1 + 1
+
+num1 = -3
+results = add_one(num1)
+print(results)
+
