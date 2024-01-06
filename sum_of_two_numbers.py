5a316fdec9a4c615c2b70fd25432a0cda2709fcf
Annie Pulzone <ajpulzone@gmail.com>
2024-01-06 08:37:53 -0800

    Create a method that adds the sum of 2 numbers

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,17 @@
+#create a function that takes two numbers as arguments and returns their sum
+
+#Ex. 
+#addition(3,2) -> 5
+#addition(-3,-6) -> -9
+# addition(7, 3) -> 10
+
+#Notes:
+#Don't forget to return the result
+
+def addition(num1, num2):
+  return num1 + num2
+
+num1 = -1
+num2 = -53
+result = addition(num1, num2)
+print(result)
